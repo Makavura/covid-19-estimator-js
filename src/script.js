a0b0f019b5851a36b61e4f012e816f8486f80c0b
Makavura <makavuramughanga@gmail.com>
2020-04-14 14:27:56 +0300

    trigger

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@ function processForm(e) {
 
   return false;
 }
+
 const form = document.getElementById('covid-19-estimator-form');
 if (form.attachEvent) {
   form.attachEvent('submit', processForm);
